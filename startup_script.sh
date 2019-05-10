48ff70c90f3ad09a4bc57f7a203b54018aff126b
Seb Bacon <seb.bacon@gmail.com>
2019-05-10 16:00:12 +0100

    Always shutdown after script execution

--- startup_script.sh
+++ startup_script.sh
@@ -1,36 +1,30 @@
 #!/bin/bash
 
-# Copyright 2015 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+set -eE  # same as: `set -o errexit -o errtrace`
 
-# [START startup_script]
+
+function shutdown(){
+    shutdown -h now
+
+
 apt-get update
-apt-get -y install git
+apt-get -y install git python3-pip
 
 cd /tmp
 git clone https://github.com/ebmdatalab/clinicaltrials-act-converter.git
 cd clinicaltrials-act-converter
 
-# Set up logging
+echo "Setting up logging"
 curl -sSO https://dl.google.com/cloudagents/install-logging-agent.sh
 sudo bash install-logging-agent.sh
 sudo cp fdaaa-converter-log.conf /etc/google-fluentd/config.d/
 sudo service google-fluentd restart
 
-# Install and run the transformer
+echo "Installing requirements"
 pip3 install -r requirements.txt
+
+echo "Running command"
 python3 load_data.py
 
 # Quit (ensures we don't pay)
-sudo shutdown -h now
+trap shudown EXIT
