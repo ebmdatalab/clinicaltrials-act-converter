43c99dc9be0454fd8a7a47cd6d07cebd404679f9
Seb Bacon <seb.bacon@gmail.com>
2019-05-16 10:34:03 +0100

    Remove long and not useful reference comment

--- ctconvert/create_instance.py
+++ ctconvert/create_instance.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-# Based on https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/compute/api/create_instance.py
-
-
 import argparse
 import os
 import time
